2e1c1c5bf1d2aec811160fd19a95eabed1ee9983
kek <oleh.petryk@gmail.com>
2017-12-14 21:36:35 +0200

    c

---
+++ js/feedback-for.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  $("#ajaxform").submit(function() {
+    var form = $(this);
+    var error = false; 
+    form.find("input, textarea").each(function() {
+      if ($(this).val() == "") {
+        alert('Please fill the field "' + $(this).attr("placeholder") + '"!'); 
+        error = true; 
+      }
+    });
+    if (!error) {
+      var data = form.serialize(); 
+      $.ajax({
+        type: "POST", 
+        url: "https://formspree.io/oleh.petryk@gmail.com", 
+        dataType: "json", 
+        data: data,
+        success: ()=> {
+            alert("Form was delivered successfully!"); 
+        },
+        error: function(xhr, ajaxOptions, thrownError) {
+          alert(xhr.status); 
+          alert(thrownError); 
+          alert("EROR!")
+        },
+      });
+    }
+    return false; 
+  });
+});
