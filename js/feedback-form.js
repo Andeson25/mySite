16bf52724490660bfd2a2de5f413b63cd7baba89
kek <oleh.petryk@gmail.com>
2017-12-15 18:08:29 +0200

    c

--- js/feedback-form.js
+++ js/feedback-form.js
@@ -1,36 +1,34 @@
-$(document).ready(function() {
-  $("#ajaxform").submit(function(e) {
-    e.preventDefault();
-    var form = $(this);
-    var errorsCount = 0;
-    form.find("input, textarea").each(function() {
-      if ($(this).val() === "") {
-        $(this).addClass("banned");
-        errorsCount++;
-      }
-    });
-    form.find("input, textarea").on("change", function() {
-      if ($(this).val() != "") {
-        $(this).removeClass("banned");
-        errorsCount--;
-      }
-    });
-    if (errorsCount === 0) {
-      var data = form.serialize();
-      $.ajax({
-        type: "POST",
-        url: "https://formspree.io/oleh.petryk@gmail.com",
-        dataType: "json",
-        data: data,
-        success: function() {
-          alert("Form was delivered!");
-        },
-        error: function(xhr, ajaxOptions, thrownError) {
-          alert(xhr.status);
-          alert(thrownError);
-        }
-      });
+$("#ajaxform").submit(function(e) {
+  e.preventDefault();
+  var form = $(this);
+  var errorsCount = 0;
+  form.find("input, textarea").each(function() {
+    if ($(this).val() === "") {
+      $(this).addClass("banned");
+      errorsCount++;
     }
-    return false;
   });
+  form.find("input, textarea").on("change", function() {
+    if ($(this).val() != "") {
+      $(this).removeClass("banned");
+      errorsCount--;
+    }
+  });
+  if (errorsCount === 0) {
+    var data = form.serialize();
+    $.ajax({
+      type: "POST",
+      url: "https://formspree.io/oleh.petryk@gmail.com",
+      dataType: "json",
+      data: data,
+      success: function() {
+        alert("Form was delivered!");
+      },
+      error: function(xhr, ajaxOptions, thrownError) {
+        alert(xhr.status);
+        alert(thrownError);
+      }
+    });
+  }
+  return false;
 });
