fd06ad9eba293a40435078345f1afb60d44f6fb7
kek <oleh.petryk@gmail.com>
2017-12-20 17:23:14 +0200

    c

--- js/sticky.js
+++ js/sticky.js
@@ -20,7 +20,14 @@
     });
 
 })(jQuery, undefined);
+$(document).ready(function () {
+    $("body").on("click", "a", function (event) {
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 1000);
+    });
 
+});
 
 
 
