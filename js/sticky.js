a88e561d79aabda14bcea33a1e1b22432d847f24
kek <oleh.petryk@gmail.com>
2017-12-20 18:23:38 +0200

    c

--- js/sticky.js
+++ js/sticky.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
     $("body").on("click", "a", function (event) {
         var id = $(this).attr('href'),
             top = $(id).offset().top;
-        $('body,html').animate({ scrollTop: top }, 1000);
+        $('body,html').animate({ scrollTop: top }, 500);
     });
 
 });
