89b34f3ab6b7336d627b3776cc1546ef5ca1e075
kek <oleh.petryk@gmail.com>
2017-12-20 16:47:39 +0200

    c

--- js/dialog.js
+++ js/dialog.js
@@ -1,8 +1,1 @@
-// var dialog = document.querySelector('dialog');
-// document.querySelector('#show').onclick = function() {
-//   dialog.show(); 
-// };
-
-// document.querySelector('#close').onclick = function() {
-//   dialog.close(); 
-// };
+\
